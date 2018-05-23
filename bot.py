8407432abb6286fe1d17f8e981619c5a61d03a68
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2018-05-23 07:45:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,13 +23,13 @@ async def on_server_join(server):
     
 if channel and channel.permissions_for(server.me).send_messages:
         
-    await channel.send('Salutations {}! I have been waiting to join you (no not rlly) hehehe. [IF I HAVE BEEN INVITED TO YOUR SERVER THAT MEANS THAT YOUR SERVER IS SPECIAL! :O] But why :thinking: ?'(server.name))
-    await channel.send('Yokido (MEEE >_<) Is an extremley early alpha access bot, availible only to a few people: -The devs close friends & -The three lucky sponsors.'(server.name))
-    await channel.send('So wait! What does that mean? IS MY OWNER THE FRIEND OF THE DEV AND I CAN USE HIM TO ACCESS EVERYTHING OMG (answer: no, tbh no-one really cares :joy:)'(server.name))
-    await channel.send('Slogan: *The bot of all the bots...the bot of the bots!* So! Now that we are done with the formalities, let us begin!'(server.name))
-    await channel.send('Honourable mentions: Mutxnts & Wiki ~ Without you guys, this could not have happened.'(server.name))
-    await channel.send('Made and scripted by Enderxcthz#1181 All rights reserved©.'(server.name))
-    await channel.send('Do "_introduction" to begin! I am allllll yours! (Totally 100% not gei :sweat_smile:)'(server.name))
+    await channel.send('Salutations {}! I have been waiting to join you (no not rlly) hehehe. [IF I HAVE BEEN INVITED TO YOUR SERVER THAT MEANS THAT YOUR SERVER IS SPECIAL! :O] But why :thinking: ?'.format(server.name))
+    await channel.send('Yokido (MEEE >_<) Is an extremley early alpha access bot, availible only to a few people: -The devs close friends & -The three lucky sponsors.'.format(server.name))
+    await channel.send('So wait! What does that mean? IS MY OWNER THE FRIEND OF THE DEV AND I CAN USE HIM TO ACCESS EVERYTHING OMG (answer: no, tbh no-one really cares :joy:)'.format(server.name))
+    await channel.send('Slogan: *The bot of all the bots...the bot of the bots!* So! Now that we are done with the formalities, let us begin!'.format(server.name))
+    await channel.send('Honourable mentions: Mutxnts & Wiki ~ Without you guys, this could not have happened.'.format(server.name))
+    await channel.send('Made and scripted by Enderxcthz#1181 All rights reserved©.'.format(server.name))
+    await channel.send('Do "_introduction" to begin! I am allllll yours! (Totally 100% not gei :sweat_smile:)'.format(server.name))
    
 
         
