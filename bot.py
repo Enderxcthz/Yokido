95a6d94e6fa996e0cb90379b9b8576ae75951e04
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2018-05-25 20:46:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ async def help(ctx):
     await bot.send_message(ctx.message.author, 'You need help? Cash me outside, HOW ABOU- no im just kidding :persevere:')
     await bot.send_message(ctx.message.author, 'https://hastebin.com/jazipaxeke.vbs')
 else:
-     print('Can\'t send message in {}. Please send owner error report.'.format(server))
+    print('Can\'t send message in {}. Please send owner error report.'.format(server))
     
 
 bot.run(os.environ["TOKEN"])
