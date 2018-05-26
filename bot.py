328e46e509bf644e3922f6163e6f534dbab0b7de
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2018-05-26 19:25:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,17 @@ async def on_ready():
 async def on_ready(): 
     await bot.change_presence(game=discord.Game(name=" with over {} servers!".format(len(bot.servers))))
     print("Successfully changed bot status!" + bot.user.name)
+    
+
+@bot.event
+async def on_message(message):
+    # Yokido sends message:
+    if message.author == bot.user:
+        return
+    if '_help' in message.content:
+        emoji = get(bot.get_all_emojis(), name=':eyes:')
+        await bot.add_reaction(message, emoji)
+        await bot.say("{} Check your DM's!".format(user.name))
  
     
 @bot.event
@@ -87,6 +98,7 @@ async def warn(ctx, user: discord.Member, reason: str = None):
 async def roast(ctx, user: discord.Member):
     messages = ['foo', 'bar', 'baz', 'bork']
     await bot.say(random.choice(messages))
+
 
 bot.remove_command('help')   
 
