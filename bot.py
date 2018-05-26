d29f503c94d8fb1bec9ccc7a46bf4d59d9b5d02e
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2018-05-26 19:52:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,9 +78,9 @@ async def unban(ctx, user: discord.Member):
     await bot.unban(user)
 
 @bot.command(pass_context=True)
-async def warn(ctx, user: discord.Member, reason: str = None):
+async def warn(ctx, server, user: discord.Member, reason: str = None):
     await bot.send_message(user, "You were warned for: {}".format(reason))
-    await bot.send_message(user, "In the {} server. This warn will be added to your record.".format(guild.name))
+    await bot.send_message(user, "In the {} server. This warn will be added to your record.".format(server.name))
     await bot.say("{} was warned successfully! :white_check_mark:".format(user.mention))
     
     
