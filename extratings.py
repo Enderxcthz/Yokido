574dbb3b222a4b8d8acaba9f9fbe70c83526ee98
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2018-05-28 16:20:20 +0100

    Update extratings.py

--- extratings.py
+++ extratings.py
@@ -2,6 +2,17 @@ import discord
 from discord.ext import commands
 import asyncio
 
+    def __init__(self, bot):
+         self.bot = bot
+
+class countdown:
+    """""    
+        
+        
+@bot.command(pass_context=True)
 async def countdown(interval, *, msg: str):
         await asyncio.sleep(int(interval))
         await bot.say(msg)
+
+def setup(bot):
+    bot.add_cog(countdown(bot))
